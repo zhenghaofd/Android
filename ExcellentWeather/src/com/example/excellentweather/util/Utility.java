530e50646705c48ba9ee019fd09efb72cc2949ae
zhenghaofd <838742806@qq.com>
2014-09-24 20:46:26 +0800

    errors still exist
    by zhenghao

---
+++ ExcellentWeather/src/com/example/excellentweather/util/Utility.java
@@ -0,0 +1,87 @@
+package com.example.excellentweather.util;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.example.excellentweather.db.WeatherDB;
+import com.example.excellentweather.model.City;
+import com.example.excellentweather.model.County;
+import com.example.excellentweather.model.Province;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+
+public class Utility {
+	
+	public synchronized static boolean handleProvincesResponse(
+			WeatherDB coolWeatherDB, String response) {
+		if (!TextUtils.isEmpty(response)) {
+			String[] allProvinces = response.split(",");
+			if (allProvinces != null && allProvinces.length > 0) {
+				for (String p : allProvinces) {
+					String[] array = p.split("\\|");
+					Province province = new Province();
+					province.setProvinceCode(array[0]);
+					province.setProvinceName(array[1]);
+					coolWeatherDB.saveProvince(province);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	public static boolean handleCitiesResponse(WeatherDB coolWeatherDB,
+			String response, int provinceId) {
+		if (!TextUtils.isEmpty(response)) {
+			String[] allCities = response.split(",");
+			if (allCities != null && allCities.length > 0) {
+				for (String c : allCities) {
+					String[] array = c.split("\\|");
+					City city = new City();
+					city.setCityCode(array[0]);
+					city.setCityName(array[1]);
+					city.setProvinceId(provinceId);
+					coolWeatherDB.saveCity(city);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static boolean handleCountiesResponse(WeatherDB coolWeatherDB,
+			String response, int cityId) {
+		if (!TextUtils.isEmpty(response)) {
+			String[] allCounties = response.split(",");
+			if (allCounties != null && allCounties.length > 0) {
+				for (String c : allCounties) {
+					String[] array = c.split("\\|");
+					County county = new County();
+					county.setCountyCode(array[0]);
+					county.setCountyName(array[1]);
+					county.setCityId(cityId);
+					coolWeatherDB.saveCounty(county);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
